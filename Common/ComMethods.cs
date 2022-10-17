f818278ca8f8a4ea04b593044ee1c025d8becfa2
Николай Николаев <lunatik2810@gmail.com>
2022-10-17 15:25:56 +0300

    Удалил ненужный метод.

--- Common/ComMethods.cs
+++ Common/ComMethods.cs
@@ -6,32 +6,11 @@ using System.Threading.Tasks;
 
 namespace Common
 {
+    /// <summary>
+    /// Статический класс для общих методов
+    /// </summary>
     public static class ComMethods
     {
-        /// <summary>
-        /// Преобразует строку String в число Int
-        /// </summary>
-        /// <param name="str">строка, которую нужно преоброзвать в число</param>
-        /// <returns>
-        /// int - успешное преобразование
-        /// null - произошла ошибка в процессе преобразования
-        /// </returns>
-        public static int? StringToInt(string str, int res)
-        {
-            try
-            {
-                bool success = int.TryParse(str, out res);
-                if (success)
-                    return res;
-                else
-                    return null;
-            }
-            catch(Exception ex)
-            {
-                ComData.logger.Error(ex, $"Ошибка в процессе преобразования строки '{str}' в число");
-                return null;
-            }            
-        }
-
+       
     }
 }
